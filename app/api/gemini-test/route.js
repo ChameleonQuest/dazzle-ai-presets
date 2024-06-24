ec87d60ecae0a772f7ae9a1c3e32f4165757c00f
Eric Hernandez <ehernandez@magicleap.com>
2024-06-24 18:54:07 -0400

    Show API key in log

--- app/api/gemini-test/route.js
+++ app/api/gemini-test/route.js
@@ -2,13 +2,14 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export async function GET(request) {
     // const { searchParams } = new URL(request.url);
+    console.log("process.env.API_KEY", process.env.API_KEY);
+
     const genAI = new GoogleGenerativeAI(process.env.API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
     const prompt = "Write a story about a magic backpack."  
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
-    console.log(text);
     
     const manifest = {
         text
