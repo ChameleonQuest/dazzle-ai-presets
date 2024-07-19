9897f65a7a1e6aa935ba305bcd3c2899fdb66c67
Eric Hernandez <ehernandez@magicleap.com>
2024-07-19 18:26:53 -0400

    Remove comments

--- app/api/gemini-caller/route.js
+++ app/api/gemini-caller/route.js
@@ -9,18 +9,6 @@ export async function POST(request) {
 
     const genAI = new GoogleGenerativeAI(process.env.API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-    // const prompt = "What's a good javascript framwork for making website? Give me a brief single suggestion with no disclaimers.";
-    // let prompt = {
-    //   "messages":[
-    //   {"role": "system", "content": "You provide concise answers, with no disclaimers. They are in the form of a rhyme."},
-    //   {"role": "user", "content": "What is a good javascript framwork for making website?"},
-    // ]}
-    // {"role": "assistant", "content": "React"},
-    // {"role": "user", "content": "Recommend an IDE for this."}
-
-    // let singleStringPrompt = prompt.messages.map(msg => `${msg.role}: ${msg.content}`).join('\n');
-    // console.log("singleStringPrompt",singleStringPrompt);
-
     const result = await model.generateContent(params.prompt);
     const response = await result.response;
     const theoutput = response.text();
--- app/gem-runner/page.js
+++ app/gem-runner/page.js
@@ -2,42 +2,21 @@
 import { Suspense, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import styles from "../page.module.css";
-// import { GoogleGenerativeAI } from "@google/generative-ai";
 
 
 function GemRunnerContent() {
-    // console.log("process.env.API_KEY", process.env.API_KEY);
-
-    // const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-    // const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
     const searchParams = useSearchParams();
     const name = searchParams.get('name');
-
-    // let singleStringPrompt = prompt.messages.map(msg => `${msg.role}: ${msg.content}`).join('\n');
-    // console.log("singleStringPrompt",singleStringPrompt);
-
-
     const [promptLog, setPromptLog] = useState({
         "messages":[
         {"role": "system", "content": "You provide concise answers, with no disclaimers. They are in the form of a rhyme."}
-        // {"role": "user", "content": "How do I make paint."}
-        // {"role": "assistant", "content": "dddd."}
     ]});
     const [newPrompt, setNewPrompt] = useState("hi");
-    // const [output, setOutput] = useState("");
     const [isGenerating, setIsGenerating] = useState(false);  
-
-    // setPromptLog({
-    //   "messages":[
-    //   {"role": "system", "content": "You provide concise answers, with no disclaimers. They are in the form of a rhyme."},
-    //   {"role": "user", "content": "What is a good javascript framwork for making website?"},
-    // ]});
 
     const handleSubmit = async () => {
         let updatedMessages = [...promptLog.messages, { role: "user", content: newPrompt }];
         // console.log("newPrompt",newPrompt,"ammendedLog",ammendedLog, "promptLog", promptLog);
-        // ammendedLog.messages.push({"role": "user", "content": newPrompt})
-        // setPromptLog({messages: updatedMessages});
         let stringPrompt = updatedMessages.map(msg => `${msg.role}: ${msg.content}`).join('\n');
         // console.log("stringPrompt", stringPrompt);
         setIsGenerating(true);
@@ -49,10 +28,8 @@ function GemRunnerContent() {
         })
         .then(response => response.json())
         .then(data => {
-            // let updatedMessages = [...promptLog.messages, { role: "assistant", content: data }];
             updatedMessages = [...updatedMessages, { role: "assistant", content: data }];
             setPromptLog({messages: updatedMessages});
-            // ammendedLog.messages.push({"role": "assistant", "content": data})
             console.log('Response:', data);
             setIsGenerating(false);
         })
