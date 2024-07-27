36645043c27314e8c6124b7115945be7803346fa
Eric Hernandez <ehernandez@magicleap.com>
2024-07-27 09:17:18 -0400

    Change file names

---
+++ app/[appName]/gem-saver/gem-saver.js
@@ -0,0 +1,63 @@
+// /app/gem-saver/page.js
+"use client";
+import { Suspense, useEffect } from 'react';
+import { useSearchParams, useParams } from 'next/navigation';
+import Link from 'next/link';
+
+function GemSaverContent() {
+const searchParams = useSearchParams();
+const { appName } = useParams();
+const description = searchParams.get('description');
+const context = searchParams.get('context');
+
+useEffect(() => {
+    const link = document.createElement('link');
+    link.rel = 'manifest';
+    link.href = `/${appName}/api/manifest`;
+    console.log("link.href", link.href);
+    document.head.appendChild(link);
+}, [appName]);
+
+useEffect(() => {
+    if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+        .register(`/${appName}/api/service-worker`, { scope: `/${appName}` })
+        .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+            console.log('Service Worker registration failed:', error);
+        });
+    });
+    }
+}, [appName]);
+
+// TODO: Detect browser and show how to save PWA on that browser.
+return (
+<div className="page-container">
+    <div className="page-content">
+        <h1>Your app is ready!!!</h1>
+        <div>please use the "Add to Home Screen" option in your browser.</div>
+        <div>
+            <strong>Name:</strong> {appName}
+        </div>
+        <div>
+            <strong>Description:</strong> {description}
+        </div>
+        <div>
+            <strong>Context:</strong> {context}
+        </div>
+        <Link href="/">Back to Home</Link>
+    </div>
+</div>
+);
+}
+
+export default function GemSaverPage() {
+return (
+    <Suspense fallback={<div>Loading...</div>}>
+    <GemSaverContent />
+    </Suspense>
+);
+}
